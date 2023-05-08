9c4037336a47605097d9babd4e443c8779c1e46f
DrRobotikk <90263786+DrRobotikk@users.noreply.github.com>
2023-05-08 12:41:02 +0100

    shit works

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,8 @@
-pakage greetings 
+package greetings
 
 import "fmt"
 
-func Hello(name string) string{
+func Hello(name string) string {
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
 }
